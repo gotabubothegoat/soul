6af992cf84bf4f8ab786bd09930979ba7b043d08
gotabubothegoat <gotabub.o@gmail.com>
2024-12-31 22:15:13 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -16,11 +16,11 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = ''
+TOKEN = '6716934871:AAEF3gEfPOM3Uiqy5wy-djEeTOzA0S7RFK8'
 MONGO_URI = 'mongodb+srv://Bishal:Bishal@bishal.dffybpx.mongodb.net/?retryWrites=true&w=majority&appName=Bishal'
-FORWARD_CHANNEL_ID = -1002156421934
-CHANNEL_ID = -1002156421934
-error_channel_id = -1002156421934
+FORWARD_CHANNEL_ID = -1002118274813
+CHANNEL_ID = -1002118274813
+error_channel_id = -1002118274813
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
